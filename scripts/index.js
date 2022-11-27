d13a58f5eb889f21bc917a352539784da943acc4
Nekhorosheva <nekhorosheva.ann@gmail.com>
2022-11-27 13:51:13 +0300

    feat:изменить часть элементов по бэм

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
-const openEditButton = document.querySelector(".button_edit"); // кнопка Редактировать профиль - открыть попап
+const openEditButton = document.querySelector(".button__type_edit"); // кнопка Редактировать профиль - открыть попап
 
 let popupBg = document.querySelector(".popup"); //подключаем бекграунд
 let popup = document.querySelector(".popup__form"); //подключаем саму форму
 let inputName = document.querySelector(".popup__field_type_name"); // находим поле ввода Имя
 let inputJob = document.querySelector(".popup__field_type_job"); //находим поле ввода О себе
-const closeEditButton = document.querySelector(".popup_closed"); //закрыть попап
+const closeEditButton = document.querySelector(".popup__closed"); //закрыть попап
 const saveEditButton = document.querySelector(".popup__form"); //выбираем форму, а не отдельную кнопку. Если несколько кнопок, выбираем по id
 
 let title = document.querySelector(".profile__title");
