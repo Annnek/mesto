b64e4090555c311fee205da96837d004ab84fb56
Nekhorosheva <nekhorosheva.ann@gmail.com>
2023-02-17 10:48:47 -0800

    bugfix: переустановить babel и обновить css

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "pages", "index.js"),
+  devtool: "inline-source-map",
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
